7a047ad08f5c944b001bc1681dbbd2101248f4ba
Umar Kabir Muktar <umarkabir3361@gmail.com>
2026-09-12 12:06:01 -0700

    Add edge runtime support to page.tsx

--- admin-dashboard/app/page.tsx
+++ admin-dashboard/app/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = (process.env.NEXT_PUBLIC_SUPABASE_URL && process.env.NEXT_PUBLIC_SUPABASE_URL.startsWith('http')) 
