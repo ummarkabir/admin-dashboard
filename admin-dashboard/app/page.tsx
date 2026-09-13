b294f489964ed1c19fb44a5c49b4047aa92b4e3a
Umar Kabir Muktar <umarkabir3361@gmail.com>
2026-09-13 02:35:36 -0700

    Add edge runtime at the top of page.tsx

--- admin-dashboard/app/page.tsx
+++ admin-dashboard/app/page.tsx
@@ -1,4 +1,5 @@
 export const runtime = 'edge';
+
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = (process.env.NEXT_PUBLIC_SUPABASE_URL && process.env.NEXT_PUBLIC_SUPABASE_URL.startsWith('http')) 
